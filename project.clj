6efe153124984dc1b66e910ee60111c1dc171115
Chris Etheridge <chris.etheridge@cognician.com>
2017-02-26 19:49:02 +0200

    Cleanup

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
-(defproject aaw "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject ether/aaa "0.1.0"
+
+  :description "Adjective-Adjective-Animal from Clojure."
+
+  :url "http://github.com/chris-etheridge/aaw"
+  
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.8.0"]])
